5f6580667b858a06c231af8eee2c6d544c291a0d
Cristian Gabriel <77484833+cristiangabriel88@users.noreply.github.com>
2024-12-25 21:48:25 +0200

    Basic 3 field caroussel + planet and signs images

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -6,10 +6,25 @@ document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("locationForm");
   const countrySelect = document.getElementById("country");
   const citySelect = document.getElementById("city");
+  const cityCache = {};
 
   if (!form || !countrySelect || !citySelect) {
-    console.error("Form, country select, or city select element is missing.");
-    return;
+    console.log("This page does not require location form functionality.");
+    return; // Exit if these elements are not present
+  }
+
+  function populateCityDropdown(cityOptions) {
+    citySelect.innerHTML = '<option value="">Select a city</option>';
+    cityOptions.forEach((city) => {
+      const option = document.createElement("option");
+      option.value = JSON.stringify({
+        name: city.name,
+        latitude: city.latitude,
+        longitude: city.longitude,
+      });
+      option.textContent = city.name;
+      citySelect.appendChild(option);
+    });
   }
 
   fetch("/static/data/countries+cities.json")
@@ -27,17 +42,26 @@ document.addEventListener("DOMContentLoaded", function () {
         const countryData = data.find((c) => c.name === selectedCountry);
 
         citySelect.innerHTML = '<option value="">Select a city</option>';
-        if (countryData && countryData.cities) {
-          countryData.cities.forEach((city) => {
-            const option = document.createElement("option");
-            option.value = JSON.stringify({
+        if (cityCache[selectedCountry]) {
+          // Use preloaded data from cache
+          populateCityDropdown(cityCache[selectedCountry]);
+        } else {
+          // Fetch cities and populate dropdown
+          const countryData = data.find(
+            (country) => country.name === selectedCountry
+          );
+          if (countryData && countryData.cities) {
+            const cityOptions = countryData.cities.map((city) => ({
               name: city.name,
               latitude: city.latitude,
               longitude: city.longitude,
-            });
-            option.textContent = city.name;
-            citySelect.appendChild(option);
-          });
+            }));
+            cityCache[selectedCountry] = cityOptions; // Cache the city data for future use
+            populateCityDropdown(cityOptions);
+          } else {
+            citySelect.innerHTML =
+              '<option value="">No cities available</option>';
+          }
         }
       });
     })
